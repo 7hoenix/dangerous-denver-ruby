0e9ca0bd0c73a94b539b6e900d3ebe51bba04b7d
philmphoenix <jpholzmann@gmail.com>
2016-01-07 08:10:19 -0700

    Everything.

---
+++ lib/index.rb
@@ -0,0 +1,23 @@
+require "csv"
+require "pry"
+
+def benchmark(start = Time.now)
+  calculate("traffic-accidents", "INCIDENT_ADDRESS")
+  calculate("traffic-accidents", "NEIGHBORHOOD_ID")
+  calculate("crime", "NEIGHBORHOOD_ID")
+  Time.now - start
+end
+
+def calculate(file, column)
+  memo = Hash.new(0)
+
+  CSV.foreach("./data/#{file}.csv", :headers => true) do |row|
+    if file == "crime"
+      memo[row["#{column}"]] += 1 if row["OFFENSE_CATEGORY_ID"] != "traffic-accident"
+    end
+  end
+
+  puts memo.sort_by { |_, v| v }.reverse.first(5)
+end
+
+puts benchmark
